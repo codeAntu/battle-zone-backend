5cda6b7609f911f99230b3a170c9e32622557ac5
Ananta <codeantu@gmail.com>
2025-03-31 22:42:48 +0530

    feat: implement login functionality with JWT authentication and user validation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import signup from "./api/auth/signup";
 import verifyOtp from "./api/auth/verifyotp";
+import login from "./api/auth/login";
 
 const app = new Hono().basePath("/api");
 
@@ -9,6 +10,7 @@ app.use("*", cors({ origin: "*" }));
 
 app.route("/", signup);
 app.route("/", verifyOtp);
+app.route("/", login);
 
 app.post("/hello", (c) => {
   return c.json({ message: "Hello, World!" });
