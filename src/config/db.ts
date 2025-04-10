a14f78bf6905d05ffd2a48316c28ff24a556695e
Ananta <codeantu@gmail.com>
2025-04-10 23:35:09 +0530

    refactor: update database connection logic to use a connection pool for improved performance

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config';
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 
-const db = drizzle(process.env.DATABASE_URL!);
+// Create a MySQL connection pool with the database URL
+const connection = mysql.createPool({
+  uri: process.env.DATABASE_URL!,
+});
+
+// Pass the MySQL connection to drizzle
+const db = drizzle(connection);
 
 export default db;
